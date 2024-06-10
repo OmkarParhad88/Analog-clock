af498fc81337395e8c0d919f87447fb49384008a
Omkar_Parhad <93907430+OmkarParhad88@users.noreply.github.com>
2024-06-10 17:27:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
-const jokes = document.getElementById("joke");
-const btn = document.getElementById("btn");
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotetion = 30 * htime + mtime / 2;
+    mrotetion = 6 * mtime;
+    srotetion = 6 * stime;
 
-// const jokesfunc = () => {
-
-//     const setHeader = {
-//         headers:{accept: 'application/json'}
-//     }
-
-//     fetch('https://icanhazdadjoke.com',setHeader)
-//     .then((res) =>  res.json())
-//     .then((deta) => joke.innerHTML = deta.joke)
-//     .catch((error) => console.log(error))
-// }
-
-// async await method
-const jokesfunc = async () => {
-
-    try {
-        const setHeader = {
-            headers: { accept: 'application/json' }
-        }
-
-        const res = await fetch('https://icanhazdadjoke.com', setHeader)
-        const deta = await res.json();
-        jokes.innerHTML = deta.joke;
-    }catch (error){
-        console.log(error)
-    }
-}
-
-btn.addEventListener('click',jokesfunc);
-jokesfunc();
+    hour.style.transform = `rotate(${hrotetion}deg)`;
+    minute.style.transform = `rotate(${mrotetion}deg)`;
+    second.style.transform = `rotate(${srotetion}deg)`;
+}, 1000);
